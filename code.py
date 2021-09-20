18d9d409ccce1ce56e2d4445541ee267c3fc4048
mightycandle <sashank.n.1711@gmail.com>
2021-09-20 19:30:56 +0530

    basic stuff only

--- code.py
+++ code.py
@@ -5,21 +5,20 @@ Created on Thu Sep  9 15:31:42 2021
 
 @author: sashank
 """
-
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
+
 data=pd.read_csv("data.csv")
 data.material=[1 if(x=="abs") else 0 for x in data.material]
 data.infill_pattern=[1 if(x=="grid") else 0 for x in data.infill_pattern]
 
-x=data.drop('elongation',axis=1)
-y=data.elongation
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.1)
+x=data.drop('strength',axis=1)
+y=data.strength
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
 LR=LinearRegression()
 LR.fit(x_train,y_train)
